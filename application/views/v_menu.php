5dc73d9b864be5e762ab13162033a54cfa6c5299
LailatulIlma18 <lailatulilma717@gmail.com>
2025-07-23 15:19:29 +0700

    membuat halaman dashboard

---
+++ application/views/v_menu.php
@@ -0,0 +1,43 @@
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="asset/dist/img/perempuan/XI PPLG 2/ILMA.JPG" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Alexander Pierce</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      
+      
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MAIN NAVIGATION</li>
+        <li class="active treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+          </a>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-files-o"></i>
+            <span>Data Master</span>
+            <span class="pull-right-container">
+              <span class="label label-primary pull-right">4</span>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Kategori</a></li>
+            <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Buku</a></li>
+            <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Ulasan</a></li>
+            <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Peminjaman</a></li>
+          </ul>
+        </li>
+        <li>
+         </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
